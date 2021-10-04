d9c2f80bce3fe4c7cbc667501216bfd0732f72d5
JoaoZati <joaozati@gmail.com>
2021-10-04 20:45:10 -0300

    User add notes implemented - first import request than get request.form.get - second make the flashs - third make the add and commit for the db

--- website/views.py
+++ website/views.py
@@ -1,10 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_user, login_required, logout_user, current_user
+from .models import Note
+from website import db
 
 views = Blueprint('views', __name__, )
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note must have at least one character', category='error')
+            return render_template('home.html', user=current_user)
+        new_note = Note(data=note, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note created with success', category='success')
+
     return render_template('home.html', user=current_user)
