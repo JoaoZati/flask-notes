23c9723242d2822cb65e41c17c54c614b219e38e
JoaoZati <joaozati@gmail.com>
2021-10-04 14:38:06 -0300

    Create run setup: - Setup the urls - make blueprints in views and auth

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '6ETfzqQUF3d5X9r5EMnLqpANpSqM9tLKFhUX62fazC'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
