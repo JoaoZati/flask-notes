a0979ec6eb4f0c6e7cb03923b99a07ff899cdd45
JoaoZati <joaozati@gmail.com>
2021-10-04 16:48:36 -0300

    Get POST Exemple: - when run it prints the dict of singup information - changes make in auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__, )
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html')
 
 
@@ -13,6 +15,6 @@ def logout():
     return render_template('home.html')
 
 
-@auth.route('/sing-up')
+@auth.route('/sing-up', methods=['GET', 'POST'])
 def sing_up():
     return render_template('sing_up.html')
