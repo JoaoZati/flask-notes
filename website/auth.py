21131c8b6e77c8b90db9bc8c66a105289500d2b5
JoaoZati <joaozati@gmail.com>
2021-10-04 17:12:08 -0300

    Flashing messages: - Making flashing mensages in base html

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__, )
 
@@ -6,7 +6,6 @@ auth = Blueprint('auth', __name__, )
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     data = request.form
-    print(data)
     return render_template('login.html')
 
 
@@ -17,4 +16,21 @@ def logout():
 
 @auth.route('/sing-up', methods=['GET', 'POST'])
 def sing_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(firstname) < 3:
+            flash('First name must be greater than 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords must be equal', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 6 characters', category='error')
+        else:
+            flash('Sing up successfully', category='success',)
+
     return render_template('sing_up.html')
